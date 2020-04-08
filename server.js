3e2627208ccd99a35b042c6df52f3fd32482fb7a
Rune Moskvil Lyngås <runely85@gmail.com>
2020-04-08 18:48:38 +0200

    Server file updated with routes and db config

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var Task = require('./api/models/todoListModel');
+var routes = require('./api/routes/todoListRoutes');
 var app = express();
 var port = process.env.PORT || 3000;
 
+// mongoose instance connection url
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/TodoDb');
+
+// bodyParser config
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// routes config
+routes(app); // register the routes
+
+// tell the server to listen on this port
 app.listen(port);
 
 console.log("Todo list RESTful API server started on port " + port);
